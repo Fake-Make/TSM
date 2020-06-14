10f8fe81a0a9005915f24cd6073213565ca84c44
Dmitry Nevada <34853028+Fake-Make@users.noreply.github.com>
2020-06-15 03:09:43 +0700

    Dataset changed

--- python/arima.py
+++ python/arima.py
@@ -5,12 +5,12 @@ from statsmodels.tsa.arima_model import ARIMA
 df = pd.read_csv('data.csv')
 
 
-# for p in range(6):
-#     for q in range(3):
-#         model = ARIMA(df.value, order=(p,1,q))
-#         model_fit = model.fit(disp=0)
-#         if (not (p == 2 and q == 4)):
-#             print(p, q, ':', model_fit.bic)
+#for p in range(3):
+#    for q in range(3):
+#        model = ARIMA(df.value, order=(p,1,q))
+#        model_fit = model.fit(disp=0)
+#        if (not (p == 2 and q == 2) and not (p == 2 and q == 3)):
+#            print(p, q, ':', model_fit.bic)
 
 # ARIMA(2, 1, 0) is the best
 model = ARIMA(df.value, order=(2, 1, 0))
